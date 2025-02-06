c97adc12a3922d4bdd020fa9015d38e8009c91b2
arpit_ciaro <arpit@ciaro.io>
2025-02-06 18:18:53 +0530

    frontend-server-url

--- Client/src/utils/socket/socket.js
+++ Client/src/utils/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const SERVER_URL = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL;
-
+console.log("Server_url", SERVER_URL);
 const socket = io(SERVER_URL, {
   transports: ["websocket", "polling"],
   withCredentials: true,

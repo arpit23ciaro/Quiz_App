c94fb4562facb3e3cf60c76c8bdfab95a474f94c
arpit_ciaro <arpit@ciaro.io>
2025-02-06 12:15:07 +0530

    socket-fix-1

--- Client/src/utils/socket/socket.js
+++ Client/src/utils/socket/socket.js
@@ -3,11 +3,8 @@ import { io } from "socket.io-client";
 const SERVER_URL = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL;
 
 const socket = io(SERVER_URL, {
-  transports: ["polling"],
+  transports: ["websocket", "polling"],
   withCredentials: true,
-  autoConnect: false, 
-  reconnection: true, 
-  reconnectionAttempts: 3, 
 });
 
 export default socket;
